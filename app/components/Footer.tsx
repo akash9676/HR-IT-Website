c20d7b279f3c596c7bde1d8a76c66ce11051b89a
Akash Gundapuneni <akashnaidu545@gmail.com>
2024-08-03 22:11:53 +0530

    updated spelling mistake

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -127,7 +127,7 @@ const Footer = () => {
                 </div>
                 <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
                     <p className="text-sm text-gray-600">
-                        © Copyright 2024 HR-IT Soluctions Inc. All rights
+                        © Copyrights 2024 HR-IT solutions Inc. All rights
                         reserved.
                     </p>
                     <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
